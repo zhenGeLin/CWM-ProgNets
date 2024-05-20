6d938f4b2676980631631e621bccc26b3176bab8
zhenGeLin <130573194+zhenGeLin@users.noreply.github.com>
2024-05-20 14:38:50 +0100

    Update send.py to TCP instead of UTP(exercise1)

--- assignment1/send.py
+++ assignment1/send.py
@@ -10,13 +10,13 @@ def randomword(length):
 
 def send_random_traffic(num_packets, interface, src_ip, dst_ip):
     dst_mac = "00:00:00:00:00:01"
-    src_mac= "00:00:00:00:00:02"
+    src_mac= "CA:FE:CA:FE:CA:FE"
     total_pkts = 0
-    port = 1024
+    port = 5555
     for i in range(num_packets):
-            data = randomword(22)
+            data = randomword(512)
             p = Ether(dst=dst_mac,src=src_mac)/IP(dst=dst_ip,src=src_ip)
-            p = p/UDP(sport= 50000, dport=port)/Raw(load=data)
+            p = p/TCP(sport= 50000, dport=port)/Raw(load=data)
             sendp(p, iface = interface, inter = 0.01)
             # If you want to see the contents of the packet, uncomment the line below
             # print(p.show())
